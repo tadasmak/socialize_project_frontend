8545cfb20f1cfa1fffeff868ef51abcc7e13ed0d
Tadas Maksimavicius <tadasmak@gmail.com>
2025-05-26 19:42:51 +0300

    Add id parameter into Activity from Router

--- src/pages/Activity.tsx
+++ src/pages/Activity.tsx
@@ -1,6 +1,11 @@
+import { useParams } from 'react-router-dom';
+
 const Activity = () => {
+    const params = useParams();
+    const id = params.id;
+
     return (
-        <h1 className="text-2xl font-bold">Activity</h1>
+        <h1 className="text-2xl font-bold">Activity {id}</h1>
     );
 };
 
