b858ffb2d46b67f20d5bf685c07fa081681e4a10
Tadas Maksimavicius <tadasmak@gmail.com>
2025-05-30 01:23:26 +0300

    Update icon and add title in navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,8 +8,9 @@ export default function Navigation() {
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="flex justify-between relative items-center h-16">
                 <div className="flex items-center inset-y-0 left-0">
-                    <div className="flex flex-1 justify-center items-stretch mr-4">
-                        <img alt="Social Eyes" src="../src/assets/branding/logo.png" className="h-8 w-auto" />
+                    <div className="flex flex-1 justify-center items-stretch mr-6">
+                        <img alt="Social Eyes" src="../src/assets/branding/logo.png" className="h-10 w-auto" />
+                        <div className="text-xl/10 font-bold ml-2">Social Eyes</div>
                     </div>
                     {navigationItems.map((item) => (
                         <div key={item.name} className={`text-white px-3 py-2 rounded-md text-sm font-medium ${item.current ? 'bg-gray-900' : 'hover:bg-gray-700'}`}>{item.name}</div>
