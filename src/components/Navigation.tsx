3c594e2ca26978e7db25cf4978056fc201455423
Tadas Maksimavicius <tadasmak@gmail.com>
2025-05-29 18:53:06 +0300

    Add logo into navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,6 +9,9 @@ export default function Navigation() {
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
                 <div className="absolute inset-y-0 left-0 flex items-center">
+                    <div className="flex items-center mr-4">
+                        <img alt="Social Eyes" src="../src/assets/branding/logo.png" className="h-8 w-auto" />
+                    </div>
                     {navigationItems.map((item) => (
                         <div key={item.name} className={`text-white px-3 py-2 rounded-md text-sm font-medium ${item.current ? 'bg-gray-900' : 'hover:bg-gray-700'}`}>{item.name}</div>
                     ))}
