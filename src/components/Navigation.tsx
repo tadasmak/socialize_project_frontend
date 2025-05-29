47874cd9015ef1050806b3316c2301f6d2e03ec2
Tadas Maksimavicius <tadasmak@gmail.com>
2025-05-29 19:56:27 +0300

    Style navigation and add profile icon as a button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,20 +1,24 @@
 const navigationItems = [
   { name: 'Activities', href: '#', current: true },
-  { name: 'Profile', href: '#', current: false }
 ]
 
 export default function Navigation() {
   return (
     <nav className="bg-gray-800">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-                <div className="absolute inset-y-0 left-0 flex items-center">
-                    <div className="flex items-center mr-4">
+            <div className="flex justify-between relative items-center h-16">
+                <div className="flex items-center inset-y-0 left-0">
+                    <div className="flex flex-1 justify-center items-stretch mr-4">
                         <img alt="Social Eyes" src="../src/assets/branding/logo.png" className="h-8 w-auto" />
                     </div>
                     {navigationItems.map((item) => (
                         <div key={item.name} className={`text-white px-3 py-2 rounded-md text-sm font-medium ${item.current ? 'bg-gray-900' : 'hover:bg-gray-700'}`}>{item.name}</div>
                     ))}
+                </div>
+                <div className="bg-gray-900 rounded-md px-2 py-1">
+                    <div className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                        <img src="../src/assets/branding/profile-icon-placeholder.svg" className="size-8 rounded-full" />
+                    </div>
                 </div>
             </div>
         </div>
