189dba6720276d92512a731173fb1cd4a0fb5945
Tadas Maksimavicius <tadasmak@gmail.com>
2025-05-30 18:57:17 +0300

    Add links to navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,7 @@
+import { Link } from 'react-router-dom';
+
 const navigationItems = [
-  { name: 'Activities', href: '#', current: true },
+  { name: 'Activities', href: '/', current: true },
 ]
 
 export default function Navigation() {
@@ -7,21 +9,21 @@ export default function Navigation() {
     <nav className="bg-yellow-950">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="flex justify-between relative items-center h-16">
-                <div className="flex items-center h-full inset-y-0 left-0">
+                <Link to="/" className="flex items-center h-full inset-y-0 left-0">
                     <div className="flex flex-1 justify-center items-stretch mr-6">
                         <img alt="Social Eyes" src="../src/assets/branding/logo.png" className="h-10 w-auto" />
                         <div className="text-xl/10 font-bold ml-2">Social Eyes</div>
                     </div>
                     {navigationItems.map((item) => (
-                        <div key={item.name} className={`flex h-full items-center px-4 text-md text-white font-medium ${item.current ? 'border-b-3 border-solid border-yellow-700' : 'hover:bg-slate-700'}`}>{item.name}</div>
+                        <Link to={item.href} key={item.name} className={`flex h-full items-center px-4 text-md text-white font-medium ${item.current ? 'border-b-3 border-solid border-yellow-700' : 'hover:bg-slate-700'}`}>{item.name}</Link>
                     ))}
-                </div>
+                </Link>
                 <div className="flex items-center">
-                    <button className="bg-yellow-800 inline-flex items-center text-sm font-medium rounded-md px-4 py-2 mr-4"><img className="h-4 mr-2" src="../src/assets/icons/plus.svg" />New Activity</button>
+                    <Link to="/activity/new" className="bg-yellow-800 inline-flex items-center text-sm font-medium rounded-md px-4 py-2 mr-4"><img className="h-4 mr-2" src="../src/assets/icons/plus.svg" />New Activity</Link>
                     <div className="rounded-md px-2 py-1">
-                        <div className="relative rounded-full p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-slate-800 focus:outline-hidden">
+                        <Link to="/profile" className="relative rounded-full p-1 text-gray-400 hover:text-white">
                             <img src="../src/assets/branding/profile-icon-placeholder.svg" className="size-8 rounded-full" />
-                        </div>
+                        </Link>
                     </div>
                 </div>
                 
