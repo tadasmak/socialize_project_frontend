5adeae253230e15f095a4559675c6dd5cbd12f16
Tadas Maksimavicius <tadasmak@gmail.com>
2025-05-30 15:29:50 +0300

    Update navigation colors

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,7 +4,7 @@ const navigationItems = [
 
 export default function Navigation() {
   return (
-    <nav className="bg-gray-800">
+    <nav className="bg-cyan-950">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="flex justify-between relative items-center h-16">
                 <div className="flex items-center inset-y-0 left-0">
@@ -13,11 +13,11 @@ export default function Navigation() {
                         <div className="text-xl/10 font-bold ml-2">Social Eyes</div>
                     </div>
                     {navigationItems.map((item) => (
-                        <div key={item.name} className={`text-white px-3 py-2 rounded-md text-sm font-medium ${item.current ? 'bg-gray-900' : 'hover:bg-gray-700'}`}>{item.name}</div>
+                        <div key={item.name} className={`text-white px-3 py-2 rounded-md text-sm font-medium ${item.current ? 'bg-slate-900' : 'hover:bg-slate-700'}`}>{item.name}</div>
                     ))}
                 </div>
-                <div className="bg-gray-900 rounded-md px-2 py-1">
-                    <div className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                <div className="bg-slate-900 rounded-md px-2 py-1">
+                    <div className="relative rounded-full p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-slate-800 focus:outline-hidden">
                         <img src="../src/assets/branding/profile-icon-placeholder.svg" className="size-8 rounded-full" />
                     </div>
                 </div>
