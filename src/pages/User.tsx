08e5225078f8793a02290c50a996d49fe2ade067
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-05 15:32:26 +0300

    Add UserType data interface into User

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,5 +1,11 @@
 import { useParams } from 'react-router-dom';
 
+interface UserType {
+    username: string;
+    personality: string;
+    age: number;
+}
+
 const User = ()  => {
     const params = useParams();
     const username = params.username;
