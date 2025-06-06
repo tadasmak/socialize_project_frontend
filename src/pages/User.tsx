a4fb777f931f247cb133bf32373c6a13d897bfec
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-06 19:29:39 +0300

    Add more data into User

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 interface UserType {
     username: string;
@@ -9,9 +10,26 @@ interface UserType {
 const User = ()  => {
     const params = useParams();
     const username = params.username;
+    const [user, setUser] = useState<UserType | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(`/api/v1/users/${username}`)
+        .then(response => response.json())
+        .then(data => setUser(data))
+        .catch(error => console.error('Error fetching user:', error))
+        .finally(() => setLoading(false));
+    }, [username])
+
+    if (loading) return <p>Loading...</p>;
+    if (!user) return <p>User not found</p>;
 
     return (
-        <h1 className="text-2xl font-bold">User {username}</h1>
+        <div>
+            <h1 className="text-2xl font-bold">User {user.username}</h1>
+            <p className="mt-2">Personality: {user.personality}</p>
+            <p className="mt-2">Age: {user.age}</p>
+        </div>
     );
 };
 
