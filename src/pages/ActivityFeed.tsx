5c4040c8a84a78dd98dc5f2fe89197da5eef9d6b
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-16 14:51:12 +0300

    Make activities images smaller in ActivityFeed

--- src/pages/ActivityFeed.tsx
+++ src/pages/ActivityFeed.tsx
@@ -46,11 +46,11 @@ const ActivityFeed: React.FC = () => {
 
             <div className="mt-6 grid justify-center grid-cols-1 lg:grid-cols-2 gap-4">
                 {activities.map((activity) => (
-                   <Link to={`/activities/${activity.id}`} key={activity.id} className="activity-container flex max-w-180 rounded-lg px-4 py-2">
-                        <div className="flex items-center w-1/3 min-w-48 aspect-square mr-4">
-                            <img src="../src/assets/activities/cycling.jpg" alt="Activity Icon" className="w-full object-cover" />
+                   <Link to={`/activities/${activity.id}`} key={activity.id} className="activity-container flex max-w-175 rounded-lg p-3">
+                        <div className="flex items-center min-w-45 w-45 aspect-square mr-4">
+                            <img src="../src/assets/activities/cycling.jpg" alt="Activity Icon" className="w-full object-cover rounded-lg" />
                         </div>
-                        <div className="p-2 w-2/3 overflow-hidden">
+                        <div className="px-2 flex-grow overflow-hidden">
                             <h2 className="font-semibold truncate text-2xl mb-1">{activity.title}</h2>
                             <p className="truncate mb-2">{activity.description}</p>
                             <p className="truncate"><span className="text-coral">Location: </span>{activity.location}</p>
