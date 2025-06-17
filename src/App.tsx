5d6abd32de03c0300da76c399681651fed36f880
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-17 17:23:58 +0300

    Reorganize routes a little bit

--- src/App.tsx
+++ src/App.tsx
@@ -21,14 +21,15 @@ const App = () => {
       <main className="mx-auto mt-12 max-w-7xl px-2 sm:px-6 lg:px-8 pb-12">
         <Routes>
           <Route path="/" element={<Home />} />
+
           <Route path="/activities" element={<ActivityFeed />} />
           <Route path="/activities/:id" element={<Activity />} />
           <Route path="/activities/new" element={<ActivityCreation />} />
-          <Route path="/users/:username" element={<User />} />
 
           <Route path="/users/register" element={<Register />} />
           <Route path="/users/login" element={<Login />} />
           <Route path="/users/me" element={<Profile />} />
+          <Route path="/users/:username" element={<User />} />
         </Routes>
       </main>
     </Router>
