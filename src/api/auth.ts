e040458d6f847a53ff85c81f6354933caaf83c2f
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-17 17:43:51 +0300

    Set up login api function

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -10,3 +10,15 @@ export const register = async (email: string, password: string) => {
     const data = await response.json();
     if (!response.ok) throw new Error(data.errors || data.error);
 }
+
+export const login = async (email: string, password: string) => {
+    const response = await fetch(`${API_URL}/sign_in`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    });
+
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.errors || data.error);
+    localStorage.setItem('authToken', data.token);
+}
