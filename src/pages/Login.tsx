e4dc5ab20a526ee30e6ee31bf2e719743cc70982
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-18 19:54:45 +0300

    Implement login page logic and toast element on success

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,46 @@
-const Login = () => {
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+
+import { login } from '../api/auth';
+
+export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            await login(email, password);
+
+            toast.success('Logged in', {
+                position: 'bottom-center',
+                autoClose: 3000,
+                pauseOnHover: true,
+                theme: 'dark',
+                className: 'bg-gradient text-white',
+            });
+
+            setTimeout(() => navigate('/activities'), 3000);
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                setError(err.message)
+            } else {
+                setError('An unexpected error occurred. Please try again.');
+            }
+        }
+    };
+
     return (
-        <div>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <input type="email" required placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="password" required placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+            <button type="submit">Login</button>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+        </form>
     );
 }
 
