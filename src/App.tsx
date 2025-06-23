09bb1e2076f7565f2844195ba0a6cc3245f28fd1
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-23 18:02:53 +0300

    Wrap the app in defined AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { AuthProvider } from './context/AuthContext';
+
 import Home from './pages/Home';
 import ActivityFeed from './pages/ActivityFeed';
 import Activity from './pages/Activity';
@@ -18,24 +20,26 @@ import Navigation from './components/Navigation';
 const App = () => {
   return (
     <Router>
-      <Navigation />
+      <AuthProvider>
+        <Navigation />
 
-      <main className="mx-auto mt-12 max-w-7xl px-2 sm:px-6 lg:px-8 pb-12">
-        <Routes>
-          <Route path="/" element={<Home />} />
+        <main className="mx-auto mt-12 max-w-7xl px-2 sm:px-6 lg:px-8 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
 
-          <Route path="/activities" element={<ActivityFeed />} />
-          <Route path="/activities/:id" element={<Activity />} />
-          <Route path="/activities/new" element={<ActivityCreation />} />
+            <Route path="/activities" element={<ActivityFeed />} />
+            <Route path="/activities/:id" element={<Activity />} />
+            <Route path="/activities/new" element={<ActivityCreation />} />
 
-          <Route path="/users/register" element={<Register />} />
-          <Route path="/users/login" element={<Login />} />
-          <Route path="/users/me" element={<Profile />} />
-          <Route path="/users/:username" element={<User />} />
-        </Routes>
-      </main>
+            <Route path="/users/register" element={<Register />} />
+            <Route path="/users/login" element={<Login />} />
+            <Route path="/users/me" element={<Profile />} />
+            <Route path="/users/:username" element={<User />} />
+          </Routes>
+        </main>
 
-      <ToastContainer />
+        <ToastContainer />
+      </AuthProvider>
     </Router>
     
   );
