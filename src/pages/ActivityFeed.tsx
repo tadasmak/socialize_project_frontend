8c08223acae3728dea39c215bc551be9a9f83ce3
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-30 16:13:00 +0300

    Remove semibold font from participants count for activities in ActivityFeed

--- src/pages/ActivityFeed.tsx
+++ src/pages/ActivityFeed.tsx
@@ -75,7 +75,7 @@ const ActivityFeed: React.FC = () => {
                             <h2 className="font-semibold truncate text-2xl mb-1">{activity.title}</h2>
                             <p className="truncate mb-2">{activity.description}</p>
                             <p className="truncate"><span className="text-coral-light">Location: </span>{activity.location}</p>
-                            <p><span className="text-coral-light">Participants: </span><span className="font-semibold">{activity.participants_count}/{activity.max_participants}</span></p>
+                            <p><span className="text-coral-light">Participants: </span>{activity.participants_count}/{activity.max_participants}</p>
                             <p><span className="text-coral-light">Age Range: </span>{activity.age_range}</p>
                             <p className="truncate"><span className="text-coral-light">Start time: </span>{new Date(activity.start_time).toLocaleString()}</p>
                         </div>
