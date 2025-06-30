a17b7df0b98726a524a8baca4076278a53d10c69
Tadas Maksimavicius <tadasmak@gmail.com>
2025-06-30 18:15:37 +0300

    Rename ActivityCreation to ActivityCreate

--- src/pages/ActivityCreate.tsx
+++ src/pages/ActivityCreate.tsx
@@ -1,7 +1,7 @@
-const ActivityCreation = () => {
+const ActivityCreate = () => {
     return (
         <h1 className="text-2xl font-bold">Activity creation</h1>
     );
 };
 
-export default ActivityCreation;
+export default ActivityCreate;
