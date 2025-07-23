8a359faf8d462c7970338ace9bb3219985a1139b
Tadas Maksimavicius <tadasmak@gmail.com>
2025-07-23 22:44:57 +0300

    Improve ActivityCard design

--- src/components/Activity/ActivityCard.tsx
+++ src/components/Activity/ActivityCard.tsx
@@ -25,13 +25,13 @@ const ActivityCard: React.FC<ActivityCardProps> = ({
     maximum_age
 }) => {
     return (
-        <Link to={`/activities/${id}`} key={id} className="bg-[#292929] ring-1 ring-black ring-opacity-5 shadow-lg flex max-w-175 rounded-lg p-3">
+        <Link to={`/activities/${id}`} key={id} className="bg-[#292929] ring-1 ring-black ring-opacity-5 shadow-lg flex rounded-lg p-3">
             <div className="flex items-center min-w-45 w-45 aspect-square mr-4">
                 <img src="../src/assets/activities/cycling.jpg" alt="Activity Icon" className="w-full object-cover rounded-lg" />
             </div>
-            <div className="px-2 flex-grow overflow-hidden">
+            <div className="px-2 flex flex-col justify-center flex-grow overflow-hidden">
                 <h2 className="font-semibold truncate text-2xl mb-1">{title}</h2>
-                <p className="truncate mb-2">{description}</p>
+                <p className="truncate text-stone-400 mb-2">{description}</p>
                 <p className="truncate"><span className="text-coral-light">Location: </span>{location}</p>
                 <p><span className="text-coral-light">Participants: </span>{participants_count}/{max_participants}</p>
                 <p><span className="text-coral-light">Age Range: </span>{minimum_age} - {maximum_age}</p>
